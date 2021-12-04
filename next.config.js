a6bf413af95d7f3171d44fb640f0aa014af8b2f9
Mr-Jayesh <jayeshpotlabattini@gmail.com>
2021-12-04 16:09:52 +0530

    reload issue solved

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png|jpeg)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
+        ],
+      },
+    ]
+  },
 }
